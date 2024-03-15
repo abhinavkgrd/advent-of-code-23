eee28f37247d16681a3344d1ebe5cb7a0a8d0fe0
Abhinav <abhinavk.grd@gmail.com>
2024-03-15 19:05:30 +0530

    day-4 attempt 1

--- src/day-4.ts
+++ src/day-4.ts
@@ -2,9 +2,10 @@ import { getLinesInFile } from "./utils";
 
 const lines = getLinesInFile("day-4");
 
-let r = 1;
 let ans = 0;
-for (const line of lines) {
+let extraCardCounts = new Array(lines.length).fill(0);
+let runningCounter = 1;
+for (const [index, line] of lines.entries()) {
   const [_, game] = line.split(":");
   const [win, my] = game.split("|");
   const winNumbers = new Set(win.split(" ").filter((x) => !!x));
@@ -15,9 +16,25 @@ for (const line of lines) {
       matchCount++;
     }
   }
+  runningCounter += extraCardCounts[index];
+  // console.log(
+  //   `game ${index + 1}: matches: ${matchCount} cnt: ${runningCounter}`
+  // );
   if (matchCount > 0) {
-    ans += Math.pow(2, matchCount - 1);
+    const stIndex = Math.min(lines.length - 1, index + 1);
+    const endIndex = Math.min(lines.length - 1, index + 1 + matchCount);
+    extraCardCounts[stIndex] += runningCounter;
+    extraCardCounts[endIndex] -= runningCounter;
   }
+  // console.log(`extraCardCounts`, extraCardCounts);
+}
+
+// console.log(`extraCardCounts`, extraCardCounts);
+
+runningCounter = 1;
+for (let extraCnt of extraCardCounts) {
+  runningCounter += extraCnt;
+  ans += runningCounter;
 }
 
 console.log("ans", ans);
