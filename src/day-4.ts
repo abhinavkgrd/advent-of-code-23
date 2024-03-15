a51ec5e76e20562eaa386372e462b58946516768
Abhinav <abhinavk.grd@gmail.com>
2024-03-15 18:10:49 +0530

    revert to day-4 part 1 solution

--- src/day-4.ts
+++ src/day-4.ts
@@ -2,17 +2,10 @@ import { getLinesInFile } from "./utils";
 
 const lines = getLinesInFile("day-4");
 
-const cntArr = new Array(lines.length + 1).fill(0);
-console.log(cntArr);
 let r = 1;
 let ans = 0;
 for (const line of lines) {
-  console.log("line", line);
-  const [cardID, game] = line.split(":");
-  console.log("cardID", cardID);
-  const [_, idString] = cardID.split(" ");
-  console.log("idString", idString);
-  const id = Number(idString);
+  const [_, game] = line.split(":");
   const [win, my] = game.split("|");
   const winNumbers = new Set(win.split(" ").filter((x) => !!x));
   const myNumbers = my.split(" ").filter((x) => !!x);
@@ -22,12 +15,9 @@ for (const line of lines) {
       matchCount++;
     }
   }
-  console.log("id", id, " cntArr[id - 1]", cntArr[id - 1]);
-  r += cntArr[id - 1];
-  ans += r;
-  cntArr[id] += r;
-  cntArr[id + matchCount] -= r;
-  console.log("r", r, "ans", ans);
+  if (matchCount > 0) {
+    ans += Math.pow(2, matchCount - 1);
+  }
 }
 
 console.log("ans", ans);
